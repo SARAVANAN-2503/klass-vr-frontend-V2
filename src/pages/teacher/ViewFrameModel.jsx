15c4f6c94dd65de80a3f0d71380b3f4a8fa5782f
dineshneurindustries <dinesh@neurindustries.com>
2025-05-27 13:03:58 +0530

    fix(webgl): build update build.no:88

--- src/pages/teacher/ViewFrameModel.jsx
+++ src/pages/teacher/ViewFrameModel.jsx
@@ -7,19 +7,19 @@ import ContentWrapper from "../../components/ContentWrapper";
 import { FaChevronLeft } from "react-icons/fa";
 
 const loaderUrl = new URL(
-  "../../../public/assets/WEBGLbuild/Build87.loader.js",
+  "../../../public/assets/WEBGLbuild/Build88.loader.js",
   import.meta.url
 ).href;
 const dataUrl = new URL(
-  "../../../public/assets/WEBGLbuild/Build87.data.unityweb",
+  "../../../public/assets/WEBGLbuild/Build88.data.unityweb",
   import.meta.url
 ).href;
 const frameworkUrl = new URL(
-  "../../../public/assets/WEBGLbuild/Build87.framework.js.unityweb",
+  "../../../public/assets/WEBGLbuild/Build88.framework.js.unityweb",
   import.meta.url
 ).href;
 const codeUrl = new URL(
-  "../../../public/assets/WEBGLbuild/Build87.wasm.unityweb",
+  "../../../public/assets/WEBGLbuild/Build88.wasm.unityweb",
   import.meta.url
 ).href;
 
